97716fa17eb4c20505c2089675e3a210ed9f5e56
tagost <guillesos@hotmail.es>
2022-01-05 19:41:23 -0300

    database

--- bd/conexion.php
+++ bd/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
         define('servidor', 'localhost');
         define('nombre_bd', 'crud_2019');
         define('usuario', 'root');
-        define('password', '');					        
+        define('password', 'root');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
