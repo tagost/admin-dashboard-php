5e1c30f225b59679226a069859b667f59288a634
root <root>
2022-03-17 15:29:12 -0300

    add enviroment variables for database conection

--- bd/conexion.php
+++ bd/conexion.php
@@ -5,7 +5,6 @@ class Conexion{
 	define('nombre_bd', getenv('DB_NAME',true));
         define('usuario', getenv('DB_USER',true));
         define('password', getenv('DB_USER_PASSWORD',true));
-	echo constant ('password');			        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
